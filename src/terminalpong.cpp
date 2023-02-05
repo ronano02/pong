9438686e14c4b96db05ca5daf18c3ae73c00024f
ronano02 <ronanoch2@gmail.com>
2023-02-05 17:59:43 +1100

    Add constructor and function outline for game controller class

--- src/terminalpong.cpp
+++ src/terminalpong.cpp
@@ -4,59 +4,65 @@
 // Ball Functions
 //------------------------------------------------------------------------------
 
-Ball::Ball(int x, int y) : mX(x), mY(y), mOriginalX(x), 
+Ball::Ball(int x, int y) : mX(x), mY(y), mOriginalX(x),
                            mOriginalY(y), mDirection(STOP) {}
 
 Ball::~Ball() {}
 
-void Ball::setDirection(eCompassDirection direction) {
+void Ball::setDirection(eCompassDirection direction)
+{
     mDirection = direction;
 }
 
-void Ball::randomiseDirection() {
+void Ball::randomiseDirection()
+{
     setDirection((eCompassDirection)((rand() % 6) + 1));
 }
 
-void Ball::move() {
-    switch (mDirection) {
-        case STOP:
-            break;
-        case NW:
-            mX--;
-            mY--;
-            break;
-        case W:
-            mX--;
-            break;
-        case SW:
-            mX--;
-            mY++;
-            break;
-        case NE:
-            mX++;
-            mY--;
-            break;
-        case E:
-            mX++;
-            break;
-        case SE:
-            mX++;
-            mY++;
-            break;
-        default:
-            std::cout << "Error: Invalid direction in Ball.move()" << std::endl;
+void Ball::move()
+{
+    switch (mDirection)
+    {
+    case STOP:
+        break;
+    case NW:
+        mX--;
+        mY--;
+        break;
+    case W:
+        mX--;
+        break;
+    case SW:
+        mX--;
+        mY++;
+        break;
+    case NE:
+        mX++;
+        mY--;
+        break;
+    case E:
+        mX++;
+        break;
+    case SE:
+        mX++;
+        mY++;
+        break;
+    default:
+        std::cout << "Error: Invalid direction in Ball.move()" << std::endl;
     }
 }
 
-void Ball::reset() {
+void Ball::reset()
+{
     mX = mOriginalX;
     mY = mOriginalY;
     mDirection = STOP;
 }
 
-std::ostream& operator<<(std::ostream& os, Ball ball) {
-    os << "Ball Position: [" << ball.mX << ", " << ball.mY << "]\n" << 
-          "Ball Direction: [" << ball.mDirection << "]\n";
+std::ostream &operator<<(std::ostream &os, Ball ball)
+{
+    os << "Ball Position: [" << ball.mX << ", " << ball.mY << "]\n"
+       << "Ball Direction: [" << ball.mDirection << "]\n";
     return os;
 }
 
@@ -68,19 +74,35 @@ Paddle::Paddle(int x, int y) : mX(x), mY(y), mOriginalX(x), mOriginalY(y) {}
 
 Paddle::~Paddle() {}
 
-void Paddle::reset() {
+void Paddle::reset()
+{
     mX = mOriginalX;
     mY = mOriginalY;
 }
 
-std::ostream& operator<<(std::ostream& os, Paddle paddle) {
+std::ostream &operator<<(std::ostream &os, Paddle paddle)
+{
     os << "Paddle Position: [" << paddle.mX << ", " << paddle.mY << "]\n";
     return os;
 }
 
+//------------------------------------------------------------------------------
+// Controller Functions
+//------------------------------------------------------------------------------
+
+Controller::Controller(int height, int width)
+{
+    mHeight = height;
+    mWidth = width;
+    quitGame = false;
+    ball = new Ball(height / 2, width / 2);
+    playerA = new Paddle(1, height / 2 - 3);
+    playerB = new Paddle(width - 2, height / 2 - 3);
+}
 
 //------------------------------------------------------------------------------
 
-int main() {
+int main()
+{
     return 0;
 }
--- src/terminalpong.hpp
+++ src/terminalpong.hpp
@@ -8,20 +8,28 @@
 #include <string>
 #include <stdlib.h>
 
-enum eCompassDirection {STOP = 0, NW, W, SW, NE, E, SE};
+enum eCompassDirection
+{
+    STOP = 0,
+    NW,
+    W,
+    SW,
+    NE,
+    E,
+    SE
+};
 
 class Ball
 {
 public:
-
     /**
-     * @brief Initializes a ball with input values
+     * @brief Consructs a ball with input values
      * @param int x - x position
      * @param int y - y position
      * @return none
      */
     Ball(int x, int y);
-    
+
     /**
      * @brief Ball destructor
      * @return none
@@ -32,38 +40,38 @@ public:
      * @brief Sets the direction of the ball to a given direction
      * @param eCompassDirection direction - the desired direction
      * @return none
-    */
+     */
     void setDirection(eCompassDirection direction);
 
     /**
      * @brief Sets the direction of the ball to a random direction
      * @return none
-    */
+     */
     void randomiseDirection();
 
     /**
      * @brief Gets the current x position of the ball
      * @return int x position
-    */
-   inline int getXPos() { return mX; }
+     */
+    inline int getXPos() { return mX; }
 
-   /**
+    /**
      * @brief Gets the current y position of the ball
      * @return int y position
-    */
-   inline int getYPos() { return mY; }
+     */
+    inline int getYPos() { return mY; }
 
-   /**
+    /**
      * @brief Gets the current direction of the ball
      * @return eCompassDirection direction
-    */
-   inline eCompassDirection getDirection() { return mDirection; }
+     */
+    inline eCompassDirection getDirection() { return mDirection; }
 
     /**
      * @brief Based on the direction of the ball, move the ball
      * @return none
-    */
-   void move();
+     */
+    void move();
 
     /**
      * @brief returns the ball to its original position and stops movement
@@ -72,13 +80,13 @@ public:
     void reset();
 
     /**
-     * @brief Writes the current position and direction of the ball 
+     * @brief Writes the current position and direction of the ball
      * to the output stream
      * @param os - output stream
      * @param ball - Ball
      * @return std::ostream&
-    */
-   friend std::ostream& operator<<(std::ostream& os, Ball ball);
+     */
+    friend std::ostream &operator<<(std::ostream &os, Ball ball);
 
 private:
     int mX;
@@ -88,12 +96,11 @@ private:
     eCompassDirection mDirection;
 };
 
-
 class Paddle
 {
 public:
     /**
-     * @brief Initializes a paddle with input values
+     * @brief Consructs a paddle with input values
      * @param int x - x position
      * @param int y - y position
      * @return none
@@ -109,44 +116,109 @@ public:
     /**
      * @brief Gets the current x position of the paddle
      * @return int x position
-    */
-   inline int getXPos() { return mX; }
+     */
+    inline int getXPos() { return mX; }
 
-   /**
+    /**
      * @brief Gets the current y position of the paddle
      * @return int y position
-    */
-   inline int getYPos() { return mY; }
+     */
+    inline int getYPos() { return mY; }
 
-   /**
+    /**
      * @brief Moves the paddle up
      * @return none
-    */
-   inline void moveUp() { mY--; }
+     */
+    inline void moveUp() { mY--; }
 
-   /**
+    /**
      * @brief Moves the paddle down
      * @return none
-    */
-   inline void moveDown() { mY++; }
+     */
+    inline void moveDown() { mY++; }
 
-   /**
+    /**
      * @brief returns the paddle to its original position
      * @return none
      */
     void reset();
 
-   /**
+    /**
      * @brief Writes the current position of the paddle to the output stream
      * @param os - output stream
      * @param paddle - Paddle
      * @return std::ostream&
-    */
-   friend std::ostream& operator<<(std::ostream& os, Paddle paddle);
+     */
+    friend std::ostream &operator<<(std::ostream &os, Paddle paddle);
 
 private:
     int mX;
     int mY;
     int mOriginalX;
     int mOriginalY;
+};
+
+class Controller
+{
+public:
+    /**
+     * @brief Consructs a game controller with given map height and width
+     * @param int height - height of map
+     * @param int width - width of map
+     * @return none
+     */
+    Controller(int height, int width);
+
+    /**
+     * @brief Controller destructor
+     * @return none
+     */
+    ~Controller();
+
+    /**
+     * @brief Adds one point to a given players score
+     * @param Paddle*player - pointer to the player
+     * @return none
+     */
+    void addPoint(Paddle *player);
+
+    /**
+     * @brief Prints the game (map, paddles, ball, etc...) to the screen
+     * @return none
+     */
+    void printGame();
+
+    /**
+     * @brief Interprets user input and moves appropriate paddle if neccessary
+     * @return none
+     */
+    void playerInput();
+
+    /**
+     * @brief Decides if the ball needs to change directions such as if it has
+     * been hit by a paddle. Also, determines if the score needs to be updated
+     * @return none
+     */
+    void gameBrain();
+
+    /**
+     * @brief Starts and runs the game loop
+     * @return none
+     */
+    void run();
+
+private:
+    int mHeight;
+    int mWidth;
+    int scoreA = 0;
+    int scoreB = 0;
+    bool quitGame;
+    Ball *ball;
+
+    Paddle *playerA;
+    Paddle *playerB;
+    char upA = 'w';
+    char downA = 's';
+    char upB = 'i';
+    char downB = 'k';
 };
